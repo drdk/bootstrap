dfb904ad34210d22e6741f1926a06cd8da82536b
XhmikosR <xhmikosr@users.sourceforge.net>
2014-03-24 08:55:22 +0200

    More updates to the JSHint options.

--- js/carousel.js
+++ js/carousel.js
@@ -154,7 +154,9 @@
 
     if ($.support.transition && this.$element.hasClass('slide')) {
       $next.addClass(type)
+      /* jshint ignore:start */
       $next[0].offsetWidth // force reflow
+      /* jshint ignore:end */
       $active.addClass(direction)
       $next.addClass(direction)
       $active
